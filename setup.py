5854a81951d45c090ac9f1aa5d09b61c906f2e7f
Daniel Neuhäuser <dasdasich@gmail.com>
2013-06-16 00:46:56 +0200

    Add tox configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='Kurrent',
+    version='0.1.0-dev',
+    license='BSD',
+    author='Daniel Neuhäuser',
+    author_email='ich@danielneuhaeuser.de',
+    packages=['kurrent'],
+    include_package_data=True
+)
