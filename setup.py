207e6276a3a7c78c7cf5e2a142393a0673834592
Daniel Neuhäuser <dasdasich@gmail.com>
2013-06-17 21:21:41 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -11,5 +11,13 @@ setup(
     url='https://github.com/DasIch/kurrent',
     packages=['kurrent'],
     include_package_data=True,
-    install_requires=['markupsafe>=0.18']
+    install_requires=['markupsafe>=0.18'],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Text Processing :: Markup'
+    ]
 )
