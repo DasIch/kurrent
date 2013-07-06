a4c9cf6ce23f374402d1e121a656cca538ae87cb
Daniel Neuhäuser <dasdasich@gmail.com>
2013-07-06 23:49:08 +0200

    Refactor cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,87 +11,77 @@ import subprocess
 import pytest
 
 
-class TestMain(object):
-    @pytest.mark.parametrize('help_option', ['-h', '--help'])
-    def test_help(self, help_option):
+class CLITest(object):
+    def execute(self, command):
         process = subprocess.Popen(
-            ['kurrent', help_option],
+            command,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
         stdout, stderr = process.communicate()
-        assert process.returncode == 0
+        return process.returncode, stdout, stderr
+
+
+class TestMain(CLITest):
+    @pytest.mark.parametrize('help_option', ['-h', '--help'])
+    def test_help(self, help_option):
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', help_option]
+        )
+        assert returncode == 0
         assert b'Usage:' in stdout
         assert b'Commands:' in stdout
         assert stderr == b''
 
     def test_version(self):
-        process = subprocess.Popen(
-            ['kurrent', '--version'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', '--version']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 0
+        assert returncode == 0
         assert stdout == b'Kurrent 0.1.0-dev\n'
         assert stderr == b''
 
     @pytest.fixture
     def help_text(self):
-        process = subprocess.Popen(
-            ['kurrent', '--help'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', '--help']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 0
+        assert returncode == 0
         assert stderr == b''
         return stdout
 
     def test_nonexisting_command(self, help_text):
-        process = subprocess.Popen(
-            ['kurrent', 'does-not-exist'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', 'does-not-exist']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 1
+        assert returncode == 1
         assert stderr == b"Error: 'does-not-exist' is not a kurrent command.\n\n"
 
         assert stdout == help_text
 
 
-class TestBuild(object):
+class TestBuild(CLITest):
     @pytest.fixture
     def help_text(self):
-        process = subprocess.Popen(
-            ['kurrent', 'build', '--help'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', 'build', '--help']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 0
+        assert returncode == 0
         assert stderr == b''
         return stdout
 
     def test_nonexisting_builder(self, help_text):
-        process = subprocess.Popen(
-            ['kurrent', 'build', 'does-not-exist', 'kurrent', 'source'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', 'build', 'does-not-exist', 'kurrent', 'source']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 1
+        assert returncode == 1
         assert stderr == b"Error: 'does-not-exist' is not a known builder.\n\n"
         assert stdout == help_text
 
     def test_nonexisting_writer(self, help_text):
-        process = subprocess.Popen(
-            ['kurrent', 'build', 'single', 'does-not-exist', 'source'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+        returncode, stdout, stderr = self.execute(
+            ['kurrent', 'build', 'single', 'does-not-exist', 'source']
         )
-        stdout, stderr = process.communicate()
-        assert process.returncode == 1
+        assert returncode == 1
         assert stderr == b"Error: 'does-not-exist' is not a known writer.\n\n"
         assert stdout == help_text
